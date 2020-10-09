fdab13a6b8009176decd4053622938beee3bfe46
Ksenia <50526165+ksenianiglas@users.noreply.github.com>
2020-10-09 17:16:33 +0300

    added function to count no  of lost games

--- functions.R
+++ functions.R
@@ -1,6 +1,7 @@
 library(RCurl)
 library(tidyverse)
 
+# read necessary data
 get_tennis_data <- function(tour, years) {
   lapply(years, function(year) {
     pth <- str_c("https://raw.githubusercontent.com/JeffSackmann/tennis_",
@@ -16,4 +17,21 @@ get_tennis_data <- function(tour, years) {
   }) %>%
     reduce(rbind) %>%
     mutate(year = str_sub(tourney_date, 1, 4))
-  }
+}
+
+# count no of games lost
+no_of_games_lost <- function(score) {
+  score %>%
+    str_split(" ")  %>%
+    lapply(function(game) {
+      str_split(game, "-") %>%
+        reduce(c) %>%
+        .[seq(2, length(.), 2)] %>%
+        str_sub(1, 1) %>%
+        as.numeric() %>%
+        sum()
+    }
+    ) %>%
+    unlist()
+}
+
